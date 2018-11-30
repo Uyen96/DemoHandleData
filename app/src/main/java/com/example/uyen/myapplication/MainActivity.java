43ae6ff5daf812ec38bfd056105b7565cc6b2a2f
uyen <honguyen2996@gmail.com>
2018-11-30 15:41:10 +0700

    data with shared preferences

--- app/src/main/java/com/example/uyen/myapplication/MainActivity.java
+++ app/src/main/java/com/example/uyen/myapplication/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.uyen.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private Button mButtonShared;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        registerListener();
+    }
+
+    private void initView() {
+        mButtonShared = findViewById(R.id.button_shared_preferences);
+    }
+
+    public void registerListener(){
+        mButtonShared.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button_shared_preferences:
+                Intent intent = new Intent(this,
+                        SharedPreferencesActivity.class);
+                startActivity(intent);
+        }
+
     }
 }
