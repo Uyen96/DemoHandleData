ffa053aefb82a61d6a80b7fc906d60b59335eef1
uyen <honguyen2996@gmail.com>
2018-12-04 10:14:07 +0700

    internal storage

--- app/src/main/java/com/example/uyen/myapplication/MainActivity.java
+++ app/src/main/java/com/example/uyen/myapplication/MainActivity.java
@@ -1,37 +1,38 @@
 package com.example.uyen.myapplication;
 
-<<<<<<< HEAD
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements OnClickListener{
 
     private Button mButtonShared;
-=======
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
->>>>>>> develop
+    private Button mButtonInternal;
+    private Button mButtonExternal;
+    private Button mButtonSqLite;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
         initView();
         registerListener();
     }
 
     private void initView() {
         mButtonShared = findViewById(R.id.button_shared_preferences);
+        mButtonInternal = findViewById(R.id.button_internal);
+        mButtonExternal = findViewById(R.id.button_external);
+        mButtonSqLite = findViewById(R.id.button_sqlite);
     }
 
     public void registerListener(){
         mButtonShared.setOnClickListener(this);
+        mButtonInternal.setOnClickListener(this);
+        mButtonExternal.setOnClickListener(this);
+        mButtonSqLite.setOnClickListener(this);
     }
 
     @Override
@@ -41,9 +42,24 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(this,
                         SharedPreferencesActivity.class);
                 startActivity(intent);
+                break;
+            case R.id.button_internal:
+                Intent intentInternal = new Intent(this,
+                        InternalStorageActivity.class);
+                startActivity(intentInternal);
+                break;
+            case R.id.button_external:
+                Intent intentExternal = new Intent(this,
+                        ExternalActivity.class);
+                startActivity(intentExternal);
+                break;
+            case R.id.button_sqlite:
+                Intent intentSqlite = new Intent(this,
+                        SqliteActivity.class);
+                startActivity(intentSqlite);
+                break;
+            default:
+                break;
         }
-
-=======
->>>>>>> develop
     }
 }
